da34916b235efb1636bbe4801219e5edafdd51e6
Pieter <pieter_jordaan@hotmail.com>
2022-08-18 09:29:47 +0000

    Merge pull request #10 from apie-lib/1.0.0

--- src/ValueObjects/IsPasswordValueObject.php
+++ src/ValueObjects/IsPasswordValueObject.php
@@ -10,8 +10,9 @@ trait IsPasswordValueObject
         $lowercase = '(?=(.*[a-z]){' . self::getMinLowercase() . ',})';
         $uppercase = '(?=(.*[A-Z]){' . self::getMinUppercase() . ',})';
         $digits = '(?=(.*[0-9]){' . self::getMinDigits() . ',})';
-        $specialCharacter = '(?=(.*[' . str_replace('\#', '#', preg_quote(self::getAllowedSpecialCharacters(), '/')) . ']){' . self::getMinSpecialCharacters() . ',})';
-        $totalSize = '.{' . self::getMinLength() . ',' . self::getMaxLength() . '}';
+        $specialCharactersRegex = str_replace('\#', '#', preg_quote(self::getAllowedSpecialCharacters(), '/'));
+        $specialCharacter = '(?=(.*[' . $specialCharactersRegex . ']){' . self::getMinSpecialCharacters() . ',})';
+        $totalSize = '[a-zA-Z0-9' . $specialCharactersRegex . ']{' . self::getMinLength() . ',' . self::getMaxLength() . '}';
         return '/^' . $lowercase . $uppercase . $digits . $specialCharacter . $totalSize . '$/';
     }
 
--- tests/Repositories/InMemoryRepositoryTest.php
+++ tests/Repositories/InMemoryRepositoryTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Apie\Tests\Core\Repositories;
 
+use Apie\CompositeValueObjects\CompositeValueObject;
 use Apie\Core\BoundedContext\BoundedContextId;
 use Apie\Core\Exceptions\EntityAlreadyPersisted;
 use Apie\Core\Exceptions\EntityNotFoundException;
@@ -20,6 +21,9 @@ class InMemoryRepositoryTest extends TestCase
      */
     public function it_can_remember_entitites()
     {
+        if (!trait_exists(CompositeValueObject::class)) {
+            $this->markTestSkipped('requires apie/composite-value-objects');
+        }
         $testItem = new InMemoryRepository(new BoundedContextId('default'));
         $this->assertEquals([], $testItem->all(new ReflectionClass(UserWithAutoincrementKey::class))->take(0, 100));
         $user = new UserWithAutoincrementKey(
@@ -41,6 +45,9 @@ class InMemoryRepositoryTest extends TestCase
      */
     public function it_throws_an_error_if_an_entity_can_not_be_found()
     {
+        if (!trait_exists(CompositeValueObject::class)) {
+            $this->markTestSkipped('requires apie/composite-value-objects');
+        }
         $testItem = new InMemoryRepository(new BoundedContextId('default'));
         $this->expectException(EntityNotFoundException::class);
         $testItem->find(new UserAutoincrementIdentifier(12));
@@ -51,6 +58,9 @@ class InMemoryRepositoryTest extends TestCase
      */
     public function it_throws_an_error_if_the_entity_can_not_be_found_that_requires_update()
     {
+        if (!trait_exists(CompositeValueObject::class)) {
+            $this->markTestSkipped('requires apie/composite-value-objects');
+        }
         $testItem = new InMemoryRepository(new BoundedContextId('default'));
         $user = new UserWithAutoincrementKey(
             new AddressWithZipcodeCheck(
