6ef2d30c7da1ac59963c5e30121c40be7d32f6e7
Pieter <pieter_jordaan@hotmail.com>
2022-08-22 12:06:24 +0000

    Merge pull request #17 from apie-lib/1.0.0

--- src/Actions/ActionInterface.php
+++ src/Actions/ActionInterface.php
@@ -12,5 +12,5 @@ interface ActionInterface
     /**
      * @param array<string|int, mixed> $rawContents
      */
-    public function __invoke(ApieContext $context, array $rawContents): mixed;
+    public function __invoke(ApieContext $context, array $rawContents): ActionResponse;
 }
---
+++ src/Actions/ActionResponseStatus.php
@@ -0,0 +1,47 @@
+<?php
+namespace Apie\Core\Actions;
+
+enum ActionResponseStatus: string
+{
+    /**
+     * Resource was properly created.
+     */
+    case CREATED = 'created';
+    /**
+     * Action was properly executed.
+     */
+    case SUCCESS = 'success';
+
+    /**
+     * Resource was properly deleted
+     */
+    case DELETED = 'deleted';
+
+    /**
+     * There is something wrong with the input.
+     * For example if it is an API CALL the request body has invalid data.
+     */
+    case CLIENT_ERROR = 'client_error';
+
+    /**
+     * {id} placeholder in route could not be found.
+     */
+    case NOT_FOUND = 'not_found';
+
+    /**
+     * There is something wrong with storing the entity.
+     *
+     * For example: database error, unique constraints, etc.
+     */
+    case PERISTENCE_ERROR = 'persistence_error';
+
+    /**
+     * There is something wrong with displaying the result of the action.
+     */
+    case OUTPUT_ERROR = 'output_error';
+
+    /**
+     * Any other error is considered a server error.
+     */
+    case SERVER_ERROR = 'server_error';
+}
