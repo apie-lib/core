67cf04f63921ed8eaa439a30ebfe3ff88103f544
Pieter <pieter_jordaan@hotmail.com>
2022-08-31 04:09:18 +0000

    Merge pull request #25 from apie-lib/1.0.0

--- src/Datalayers/Lists/PaginatedResult.php
+++ src/Datalayers/Lists/PaginatedResult.php
@@ -5,11 +5,14 @@ use Apie\Core\Datalayers\Search\QuerySearch;
 use Apie\Core\Datalayers\ValueObjects\LazyLoadedListIdentifier;
 use Apie\Core\Entities\EntityInterface;
 use Apie\Core\Lists\ItemList;
+use Iterator;
+use IteratorAggregate;
 
 /**
  * @template T of EntityInterface
+ * @implements IteratorAggregate<int, T>
  */
-final class PaginatedResult
+final class PaginatedResult implements IteratorAggregate
 {
     /**
      * @param LazyLoadedListIdentifier<T> $id
@@ -23,5 +26,10 @@ final class PaginatedResult
         public readonly int $pageSize,
         public readonly QuerySearch $querySearch
     ) {
+    }
+
+    public function getIterator(): Iterator
+    {
+        return $this->list->getIterator();
     }
 }
