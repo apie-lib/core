58a3b1f27f3571ad73680836f4d0fb736e871d99
pjordaan <pjordaan@users.noreply.github.com>
2023-01-03 15:57:22 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

--- src/BoundedContext/BoundedContextHashmap.php
+++ src/BoundedContext/BoundedContextHashmap.php
@@ -1,7 +1,10 @@
 <?php
 namespace Apie\Core\BoundedContext;
 
+use Apie\Core\Entities\EntityInterface;
+use Apie\Core\Identifiers\IdentifierInterface;
 use Apie\Core\Lists\ItemHashmap;
+use ReflectionClass;
 
 /**
  * Contains multiple bounded contexts mapped by key.
@@ -13,5 +16,31 @@ final class BoundedContextHashmap extends ItemHashmap
     public function offsetGet(mixed $offset): BoundedContext
     {
         return parent::offsetGet($offset);
+    }
+
+    /**
+     * @param ReflectionClass<EntityInterface|IdentifierInterface<EntityInterface>> $class
+     */
+    public function getBoundedContextFromClassName(ReflectionClass $class, ?BoundedContextId $prio = null): ?BoundedContext
+    {
+        if ($class->implementsInterface(IdentifierInterface::class)) {
+            $class = $class->getMethod('getReferenceFor')->invoke(null);
+        }
+        if ($prio && isset($this[$prio->toNative()])) {
+            $boundedContext = $this[$prio->toNative()];
+            foreach ($boundedContext->resources as $resource) {
+                if ($resource->name === $class->name) {
+                    return $boundedContext;
+                }
+            }
+        }
+        foreach ($this as $boundedContext) {
+            foreach ($boundedContext->resources as $resource) {
+                if ($resource->name === $class->name) {
+                    return $boundedContext;
+                }
+            }
+        }
+        return null;
     }
 }
