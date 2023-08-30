27da6a3969252f02bee43e59e87def9144d4d208
pjordaan <pjordaan@users.noreply.github.com>
2023-08-30 14:20:46 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

---
+++ src/ApieLib.php
@@ -0,0 +1,14 @@
+<?php
+namespace Apie\Core;
+
+final class ApieLib
+{
+    /**
+     * @codeCoverageIgnore
+     */
+    private function __construct()
+    {
+    }
+
+    public const VERSION = '1.0-dev';
+}
