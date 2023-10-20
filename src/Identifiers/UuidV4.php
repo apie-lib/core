eac9db3756246c44e36a4719e5ed306918a80883
pjordaan <pjordaan@users.noreply.github.com>
2023-10-20 07:08:42 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

--- src/Identifiers/UuidV4.php
+++ src/Identifiers/UuidV4.php
@@ -7,6 +7,9 @@ use Ramsey\Uuid\Uuid as RamseyUuid;
 #[FakeMethod("createRandom")]
 class UuidV4 extends Uuid
 {
+    /**
+     * @return static
+     */
     public static function createRandom(): self
     {
         return new static(RamseyUuid::uuid4()->toString());
