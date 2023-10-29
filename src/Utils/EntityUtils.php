e9101a3abb8bab48e31c0bea57bf92af1937b8ea
pjordaan <pjordaan@users.noreply.github.com>
2023-10-29 13:48:26 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

--- src/Utils/EntityUtils.php
+++ src/Utils/EntityUtils.php
@@ -3,6 +3,7 @@ namespace Apie\Core\Utils;
 
 use Apie\Core\Entities\EntityInterface;
 use Apie\Core\Entities\PolymorphicEntityInterface;
+use Apie\Core\Exceptions\IndexNotFoundException;
 use Apie\Core\Other\DiscriminatorMapping;
 use ReflectionClass;
 use ReflectionMethod;
@@ -74,9 +75,40 @@ final class EntityUtils
             $mapping = $current->getMethod('getDiscriminatorMapping')->invoke(null);
             $config = $mapping->getConfigForClass($entity);
             $result[$mapping->getPropertyName()] = $config->getDiscriminator();
-            $current = new ReflectionClass($config->getClassName());
             $last = $current->getMethod('getDiscriminatorMapping')->getDeclaringClass()->name;
+            $current = new ReflectionClass($config->getClassName());
         }
         return $result;
+    }
+
+    /**
+     * Converts discriminator mappings from an array into the linked class.
+     *
+     * @template T of PolymorphicEntityInterface
+     * @param array<string, string> $discriminators
+     * @param ReflectionClass<T> $base
+     * @return ReflectionClass<T>
+     */
+    public static function findClass(array $discriminators, ReflectionClass $base): ReflectionClass
+    {
+        /** @var DiscriminatorMapping $mapping */
+        $mapping = $base->getMethod('getDiscriminatorMapping')->invoke(null);
+        $value = $discriminators[$mapping->getPropertyName()] ?? null;
+        if (!isset($value)) {
+            throw new IndexNotFoundException($mapping->getPropertyName());
+        }
+        $current = new ReflectionClass($mapping->getClassNameFromDiscriminator($value));
+        $last = $base->name;
+        while ($current->getMethod('getDiscriminatorMapping')->getDeclaringClass()->name !== $last) {
+            $mapping = $base->getMethod('getDiscriminatorMapping')->invoke(null);
+            $value = $discriminators[$mapping->getPropertyName()] ?? null;
+            if (!isset($value)) {
+                throw new IndexNotFoundException($mapping->getPropertyName());
+            }
+            $last = $current->getMethod('getDiscriminatorMapping')->getDeclaringClass()->name;
+            $current = new ReflectionClass($mapping->getClassNameFromDiscriminator($value));
+        }
+
+        return $current;
     }
 }
