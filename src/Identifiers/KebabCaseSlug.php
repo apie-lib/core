57ce8b5b77ebc3e8bc928e9cd60dbc67799e468c
pjordaan <pjordaan@users.noreply.github.com>
2023-11-16 15:53:58 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

--- src/Identifiers/KebabCaseSlug.php
+++ src/Identifiers/KebabCaseSlug.php
@@ -3,6 +3,8 @@ namespace Apie\Core\Identifiers;
 
 use Apie\Core\ValueObjects\Interfaces\HasRegexValueObjectInterface;
 use Apie\Core\ValueObjects\IsStringWithRegexValueObject;
+use ReflectionClass;
+use ReflectionMethod;
 
 /**
  * Indicate an identifier written with dashes (kebab-case).
@@ -10,6 +12,16 @@ use Apie\Core\ValueObjects\IsStringWithRegexValueObject;
 class KebabCaseSlug implements HasRegexValueObjectInterface
 {
     use IsStringWithRegexValueObject;
+
+    /**
+     * @param ReflectionClass<object>|ReflectionMethod $class
+     */
+    public static function fromClass(ReflectionClass|ReflectionMethod $class): self
+    {
+        $shortName = $class instanceof ReflectionClass ? $class->getShortName() : $class->name;
+        $short = preg_replace('/([a-z])([A-Z])/', '$1-$2', $shortName);
+        return static::fromNative(strtolower($short));
+    }
 
     public static function getRegularExpression(): string
     {
