9b61e9a16838f08259a3afc25e0e52b7a4378f4d
pjordaan <pjordaan@users.noreply.github.com>
2024-03-30 07:47:51 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

---
+++ src/ValueObjects/Interfaces/AllowsLargeStringsInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace Apie\Core\ValueObjects\Interfaces;
+
+/**
+ * Marker interface for value objects to tell data layer that it can contain large strings
+ */
+interface AllowsLargeStringsInterface
+{
+}
