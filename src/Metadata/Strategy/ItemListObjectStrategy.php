43310d2dd230d9803eb0f3b0324786a120260e9a
pjordaan <pjordaan@users.noreply.github.com>
2024-05-11 22:11:49 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

--- src/Metadata/Strategy/ItemListObjectStrategy.php
+++ src/Metadata/Strategy/ItemListObjectStrategy.php
@@ -3,6 +3,7 @@ namespace Apie\Core\Metadata\Strategy;
 
 use Apie\Core\Context\ApieContext;
 use Apie\Core\Lists\ItemList;
+use Apie\Core\Lists\ItemSet;
 use Apie\Core\Metadata\ItemListMetadata;
 use Apie\Core\Metadata\StrategyInterface;
 use Apie\Core\Utils\HashmapUtils;
@@ -12,11 +13,11 @@ final class ItemListObjectStrategy implements StrategyInterface
 {
     public static function supports(ReflectionClass $class): bool
     {
-        return HashmapUtils::isList($class);
+        return HashmapUtils::isList($class) || HashmapUtils::isSet($class);
     }
 
     /**
-     * @param ReflectionClass<ItemList> $class
+     * @param ReflectionClass<ItemList|ItemSet> $class
      */
     public function __construct(private readonly ReflectionClass $class)
     {
--- src/Utils/HashmapUtils.php
+++ src/Utils/HashmapUtils.php
@@ -3,6 +3,7 @@ namespace Apie\Core\Utils;
 
 use Apie\Core\Lists\ItemHashmap;
 use Apie\Core\Lists\ItemList;
+use Apie\Core\Lists\ItemSet;
 use Apie\TypeConverter\ReflectionTypeFactory;
 use ReflectionClass;
 use ReflectionMethod;
@@ -46,5 +47,14 @@ final class HashmapUtils
     {
         $class = ConverterUtils::toReflectionClass($input);
         return $class !== null && ($class->name === ItemList::class || $class->isSubclassOf(ItemList::class));
+    }
+
+    /**
+     * @param string|ReflectionClass<object>|ReflectionProperty|ReflectionType|ReflectionMethod $input
+     */
+    public static function isSet(string|ReflectionClass|ReflectionProperty|ReflectionType|ReflectionMethod $input): bool
+    {
+        $class = ConverterUtils::toReflectionClass($input);
+        return $class !== null && ($class->name === ItemSet::class || $class->isSubclassOf(ItemSet::class));
     }
 }
