29e9b4f2c973caaf434cd574d63841d3345e3861
pjordaan <pjordaan@users.noreply.github.com>
2024-05-25 09:36:02 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

--- src/Metadata/ValueObjectMetadata.php
+++ src/Metadata/ValueObjectMetadata.php
@@ -3,16 +3,30 @@ namespace Apie\Core\Metadata;
 
 use Apie\Core\Context\ApieContext;
 use Apie\Core\Context\MetadataFieldHashmap;
+use Apie\Core\Dto\ValueOption;
 use Apie\Core\Enums\ScalarType;
 use Apie\Core\Lists\StringList;
-use Apie\Core\Metadata\Concerns\NoValueOptions;
+use Apie\Core\Lists\ValueOptionList;
+use Apie\Core\ValueObjects\Interfaces\LimitedOptionsInterface;
 use Apie\Core\ValueObjects\Interfaces\ValueObjectInterface;
+use Apie\Core\ValueObjects\Utils;
 use ReflectionClass;
 
 class ValueObjectMetadata implements NullableMetadataInterface
 {
     // TODO: check regex to see if the options are limited?
-    use NoValueOptions;
+    public function getValueOptions(ApieContext $context, bool $runtimeFilter = false): ?ValueOptionList
+    {
+        if (in_array(LimitedOptionsInterface::class, $this->class->getInterfaceNames())) {
+            //$translator = $context->getContext(ApieTranslator::class, false) ? : new ApieTranslator();
+            $options = [];
+            foreach ($this->class->getMethod('getOptions')->invoke(null) as $option) {
+                $options[] = new ValueOption(Utils::toString($option), $option);
+            }
+            return new ValueOptionList($options);
+        }
+        return null;
+    }
     /**
      * @param ReflectionClass<ValueObjectInterface> $class
      */
