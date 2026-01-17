4d0e6b1c228f993d373146894324709fafd443fd
pjordaan <pjordaan@users.noreply.github.com>
2026-01-17 16:22:58 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

--- src/BackgroundProcess/BackgroundProcessStatus.php
+++ src/BackgroundProcess/BackgroundProcessStatus.php
@@ -1,10 +1,16 @@
 <?php
 namespace Apie\Core\BackgroundProcess;
 
+use Apie\Core\Attributes\Description;
+
 enum BackgroundProcessStatus: string
 {
+    #[Description('The background process has started and is running')]
     case Active = 'active';
+    #[Description('The background process has finished successfully')]
     case Finished = 'finished';
+    #[Description('The background process has finished with errors even after retries')]
     case TooManyErrors = 'tooManyErrors';
+    #[Description('The background process has been canceled')]
     case Canceled = 'canceled';
 }
