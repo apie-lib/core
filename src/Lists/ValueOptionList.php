27d317bb05aaddaca892ed482093e19aad84fba8
pjordaan <pjordaan@users.noreply.github.com>
2026-07-02 16:18:15 +0000

    Update file(s) "packages/core/." from "apie-lib/apie-lib-monorepo"

--- src/Lists/ValueOptionList.php
+++ src/Lists/ValueOptionList.php
@@ -3,12 +3,31 @@ namespace Apie\Core\Lists;
 
 use Apie\Core\Dto\ValueOption;
 
-class ValueOptionList extends ItemList
+final class ValueOptionList extends ItemList
 {
     protected bool $mutable = false;
+
+    private bool $integerKeys;
+
+    public function hasIntegerKeys(): bool
+    {
+        if (isset($this->integerKeys)) {
+            return $this->integerKeys;
+        }
+        foreach ($this->internal as $value) {
+            if (preg_match('/^\d+$/', $value->name)) {
+                $this->integerKeys = true;
+                return true;
+            }
+        }
+        $this->integerKeys = false;
+        return false;
+    }
 
     public function offsetGet(mixed $offset): ValueOption
     {
         return parent::offsetGet($offset);
     }
+
+
 }
